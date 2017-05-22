a5f11b2b4f6789429e2045eb132db30ffdd40810
Jordan Benner <jordanbenner@outlook.com>
2017-05-22 12:59:21 -0500

    Exercise 4

---
+++ jquery4.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#clear-button').click(function(){
+      $(this).click(function(){
+          $('input[type="text"]').val('');
+      });
+  });
+  $('#save-button').click(function(){
+    var input = $("input[name='checkListItem']").val();
+    $('.list').append('#text-field')
+    $('#log').append( '<li>' + input + '</li>');
+  });
+
+
+
+
+})
