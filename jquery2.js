34b1269daab82170e4d287b9d7251d0038d1dd38
Jordan Benner <jordanbenner@outlook.com>
2017-05-22 11:12:33 -0500

    Exercise 3

--- jquery2.js
+++ jquery2.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('#the-button').click(function() {
     $(this).text('You clicked me!');
-  })
-})
+  });
 
-$('#the-button').click(function() {
-  alert('You clicked me!');
+  $('#the-button').click(function() {
+    alert('You clicked me!');
+  });
 })
---
+++ jquery3.js
@@ -0,0 +1,8 @@
+  $(document).ready(function() {
+    $('#the-button').click(function() {
+      $(this).text('Click Again?');
+        $('#the-message').text('You clicked me!');
+    });
+
+
+  })
